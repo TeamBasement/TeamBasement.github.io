e7c8f128b7316ca66deaf5a26795085d978d6d59
Alexander Bake <alexbake7@gmail.com>
2020-10-14 19:07:46 -0500

    Publishing lover-of-wandering to site, from commit: f0a27e1d75c957d84b91e5aa2b2cd97204f64333

--- projects/lover-of-wandering/src.f69400ca.js
+++ projects/lover-of-wandering/src.f69400ca.js
@@ -63870,7 +63870,7 @@ function () {
           affect: _this.dispatchEffect.bind(_this),
           state: state || _this.state
         });
-        return state;
+        return _this.run();
       });
     }
 
@@ -65883,7 +65883,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55498" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58851" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
